0502f18b224cbe1824131be637dd0506498343ae
brandon <brandon.kessler@annalect.com>
2021-11-23 17:18:28 -0800

    updated config settings for vscode

--- scripts/env_config_setup.py
+++ scripts/env_config_setup.py
@@ -6,7 +6,7 @@ def env_config_setup(envdir):
 
     env_config = {'envdir': envdir}
 
-    with open(os.path.join(envdir, 'env_config.json'), 'w') as env_json:
+    with open(os.path.join(envdir, 'config', 'env_config.json'), 'w') as env_json:
         json.dump(env_config, env_json)
 
     print("\nConfig files have been created in environment_setup and Environment\n")
---
+++ scripts/vscode_workspace.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+def vscode_workspace(envdir):
+	main_folders = ['code', 'config', 'data', 'notes', 'work']
+
+	setup = {
+		"folders": [],
+		"settings": {
+			"workbench.colorTheme": "Night Owl (No Italics)",
+			"workbench.iconTheme": "vscode-icons"
+		},
+		"extensions": {
+			"recommendations": [
+				"ms-python.vscode-pylance",
+				"ms-python.python",
+				"yzhang.markdown-all-in-one",
+				"sdras.night-owl",
+				"vscode-icons-team.vscode-icons",
+				"dracula-theme.theme-dracula"
+			]
+		},
+	}
+
+	# Add the environment_setup folder
+	if os.getcwd().split(os.sep)[-1] == 'environment_setup':
+		setup['folders'].append({"path": os.getcwd()})
+	elif os.path.exists(os.path.join(os.getcwd(), 'environment_setup')):
+		structure = os.path.join(os.getcwd(), 'environment_setup')
+		setup['folders'].append({"path": structure})
+	else:
+		print("Unable to find environment_setup directory. Not added to workspace.")
+	
+
+	# Add all of the Environment folders
+	for folder in main_folders:
+		add_folder = {
+			"path": os.path.join(envdir, folder)
+		}
+		setup['folders'].append(add_folder)
+
+	# Write the json config to environment.code-workspace file in config folder
+	with open(os.path.join(envdir, 'config', 'environment.code-workspace'), 'w') as f:
+		f.write(json.dumps(setup, indent=4))
+
+vscode_workspace('/Users/brandon.kessler/Environment')
+
